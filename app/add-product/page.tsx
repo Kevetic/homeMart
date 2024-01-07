79905391b544a6667c845dab73e3c99a0d2fb3de
Kevetic <Kevetic@gmail.com>
2024-01-07 16:28:04 -0600

    settings

---
+++ app/add-product/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddProductPage() {
+  return (
+    <div>
+      <h1 className="text-lg hover:text-4xl font-bold mb-3">Add Product</h1>
+    </div>
+  );
+}
+
+export default AddProductPage;
