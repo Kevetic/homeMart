e7bd510ba52e6ba819b46381fe34867c709019f0
Kevetic <Kevetic@gmail.com>
2024-01-07 21:47:33 -0600

    settings are now proper and form built, no function

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -1,9 +1,44 @@
 import React from "react";
 
+export const metadata = {
+  title: "Add Product",
+};
+
 function AddProductPage() {
   return (
     <div>
-      <h1 className="text-lg hover:text-4xl font-bold mb-3">Add Product</h1>
+      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+      <form>
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className="input input-bordered mb-3 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea textarea-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="imgUrl"
+          placeholder="Image URL"
+          type="url"
+          className="input input-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className="input input-bordered mb-3 w-full"
+        />
+        <button type="submit" className="btn btn-primary btn-block">
+          Add Product
+        </button>
+      </form>
     </div>
   );
 }
